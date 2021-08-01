20a9e119a9b7f2b4b3187dd53d58bb867be66ec2
kiranravindrant <85882250+kiranravindrant@users.noreply.github.com>
2021-08-01 22:25:16 +0530

    Initial commit

---
+++ Program (2).cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AddressbookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book Program ");
+
+            while (true)
+            {
+                Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
+                    " \n 3 for editing the existing contact \n 4 for deleting contact\n 5 for  Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddMultipleContact.AddMember();
+                        
+                       // AddAndRetrieveContact obj = new AddAndRetrieveContact();
+                       //obj.EnterInput();
+                       break;
+
+
+                    case 2:
+                        AddMultipleContact.ViewContacts();
+                    //AddAndRetrieveContact obj1 = new AddAndRetrieveContact();
+                    //obj1.ViewTheDetails();
+                    break;
+
+
+
+                    case 3:
+                        AddMultipleContact.EditDetails();
+                        //EditTheDetails obj2 = new EditTheDetails();
+                        //obj2.EditUsingName();
+                        break;
+
+                    case 4:
+                        // Delete obj3 = new Delete();
+                        //obj3.DeletetheName();
+                        AddMultipleContact.DeleteDetails();
+                        break;
+
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Enter valid choice.");
+                        break;
+
+                        
+                }
+            }
+        }
+    }
+}
+
+
+        
+    
+
---
+++ UC_2_AddAndRetrieveContact.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressbookProgram
+{
+     public class AddAndRetrieveContact
+    {
+        public static List<Contact> AddressBook = new List<Contact>();
+
+        public void EnterInput()
+        {
+            Contact objcontact = new Contact();
+
+            Console.Write("Enter the First Name : ");
+            objcontact.FirstName = Console.ReadLine();
+
+
+            Console.Write("Enter the last Name  : ");
+            objcontact.LastName = Console.ReadLine();
+
+            Console.Write("Enter the address  :");
+            objcontact.Address = Console.ReadLine();
+
+            Console.Write("Enter the City  :");
+            objcontact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the state  : ");
+            objcontact.State = Console.ReadLine();
+
+            while (true)
+            {
+                Console.Write("Enter Zip Code  : ");
+                string tempv = Console.ReadLine();
+
+                if (tempv.Length == 6)
+                {
+                    objcontact.Zip = tempv;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter Your Phone Number: ");
+                string tempv2 = Console.ReadLine();
+
+                if (tempv2.Length == 10)
+                {
+                    objcontact.PhoneNumber = tempv2;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid 10 digit Phone Number.");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter Your Email Address: ");
+                string tempv3 = Console.ReadLine();
+
+                if (tempv3.Contains("@"))
+                {
+                    objcontact.Email = tempv3;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid Email Address.");
+                }
+            }
+
+            AddressBook.Add(objcontact);
+
+            Console.WriteLine("..................");
+
+        }
+
+
+
+        public void ViewTheDetails()
+        {
+            if (AddressBook.Count > 0)
+            {
+                foreach ( var item in AddressBook)
+                {
+                    PrintTheDetails(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Address book is empty! ");
+
+            }
+        }
+
+        public void PrintTheDetails(Contact item)
+        {
+            Console.WriteLine("First Name :   " + item.FirstName);
+            Console.WriteLine("Last Name :    " + item.LastName);
+            Console.WriteLine("Address :      " + item.Address);
+            Console.WriteLine("City    :      " + item.City);
+            Console.WriteLine("State   :      " + item.State);
+            Console.WriteLine("Zip     :      " + item.Zip);
+            Console.WriteLine("Phone Number  : " + item.PhoneNumber);
+            Console.WriteLine("email  :       " + item.Email);
+
+
+        }
+
+
+    }
+}
