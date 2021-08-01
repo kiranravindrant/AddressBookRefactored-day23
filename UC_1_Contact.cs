fe30b11dfb49951fde9ccda779d8065d8ce6c6b2
kiranravindrant <85882250+kiranravindrant@users.noreply.github.com>
2021-08-01 22:25:16 +0530

    Initialize UC2

---
+++ UC_1_Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressbookProgram
+{
+   public  class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
