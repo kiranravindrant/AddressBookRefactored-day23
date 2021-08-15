e8c8e4550bd4de79e44c11716fa3e3c9c2ae2bf3
kiranravindrant <kiranravindrant@gmail.com>
2021-08-15 17:21:57 +0530

    [kiran]Added:BinarySerialization class to store data in binary

---
+++ BinarySerialization.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace AddressbookProgram
+{ [Serializable]
+    public class BinarySerialization
+    {
+
+         public static void SerializeToBinary()
+         {       
+                string path = @"C:\Users\LENOVO\source\repos\Addressbook\inbinary.txt";
+                FileStream obstream = new FileStream(path, FileMode.OpenOrCreate);
+
+            BinaryFormatter obformatter = new BinaryFormatter();
+
+            obformatter.Serialize(obstream,MultipleAddressBook.AddressBookDictionary);
+            obstream.Close();
+            Console.ForegroundColor = System.ConsoleColor.Cyan;
+            Console.WriteLine("File serialized and saved in path :"+path);
+            Console.ReadLine();
+
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+
+}
