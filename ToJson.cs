f2e649e57acd3400c9e48ba1fff4fd68a0c993a1
kiranravindrant <kiranravindrant@gmail.com>
2021-08-15 13:37:50 +0530

    [kiran]Added:Json Read write Methods

---
+++ ToJson.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections;
+
+
+namespace AddressbookProgram
+{
+    public class ToJson
+    {
+        public static void JsonWrite()
+        {
+            string strResultJson = JsonConvert.SerializeObject(MultipleAddressBook.AddressBookDictionary);
+            File.WriteAllText(@"AddressBook.json", strResultJson);
+            Console.WriteLine("Stored");
+
+        
+        }
+   
+        public static void ReadJson()
+        {
+            string ContentJson = File.ReadAllText(@"AddressBook.json");
+            var result = JsonConvert.DeserializeObject<IDictionary>(ContentJson);
+           
+            foreach (DictionaryEntry entry  in result)
+            {
+
+                Console.WriteLine(entry.Key+" : "+ entry.Value);    
+            }
+        
+        }
+        
+    
+    }
+
+   
+}
