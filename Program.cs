8ac640007f2f0fa23d47b806b4b3d73ecfd2c361
kiranravindrant <kiranravindrant@gmail.com>
2021-08-15 13:35:52 +0530

    [kiran]Updated :Main method to incorporate Tojson class functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 namespace AddressbookProgram
 {
     class Program
@@ -13,7 +13,7 @@ namespace AddressbookProgram
             while (true)
             {
                 Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
-                    " \n 3 for editing the existing contact \n 4 for deleting contact\n 5 for  Search by city \n 6  Search by state  \n 7  Exit \n 8  Sort A->z \n 9 SortByCity_state");
+                    " \n 3 for editing the existing contact \n 4 for deleting contact\n 5 for  Search by city \n 6  Search by state  \n 7  Exit \n 8  Sort A->z \n 9 SortByCity_state \n 10 Write to json \n 11 Read From Json ");
 
                 try
                 {
@@ -71,8 +71,13 @@ namespace AddressbookProgram
                         case 9:
                             objnew.SortByCityStateZip();
                             break;
+                        case 10:
+                            ToJson.JsonWrite();
+                            break;
 
-
+                        case 11:
+                            ToJson.ReadJson();
+                            break;
 
                         default:
                             Console.WriteLine("Enter valid choice.");
@@ -89,8 +94,8 @@ namespace AddressbookProgram
                 }
 
 
-
-
+                
+                
 
                 
 
